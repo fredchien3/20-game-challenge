37a376c8dbdfb7e302ad0d27fe1fb71fd35f3fab
Fred Chien <fred.chien3@gmail.com>
2025-08-04 21:40:30 -0400

    Add Pillar, collision with Fish

--- fish.gd
+++ fish.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal death
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -7,8 +8,14 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
+	
 
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
-
+	
 	move_and_slide()
+
+func on_collision():
+	print("ouch!")
+	death.emit()
+	queue_free()
