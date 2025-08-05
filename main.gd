45f2c51c2898905a33027639d33bc4a5ca218f03
Fred Chien <fred.chien3@gmail.com>
2025-08-04 22:14:39 -0400

    Add Obstacle spawning (renamed from Pillar)

--- main.gd
+++ main.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
+@export var obstacle_scene: PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+# Spawns an obstacle at a random location to the right of the screen
+func _on_obstacle_spawn_timer_timeout() -> void:
+	var obstacle = obstacle_scene.instantiate()
+	
+	var viewport_width = get_viewport().size[0]
+	var viewport_height = get_viewport().size[1]
+ 
+	var spawn_x = (viewport_width / 2) + 60
+	var y_bound = (viewport_height / 2)
+	var spawn_y = randf_range(y_bound, -y_bound)
+	
+	obstacle.position = Vector2(spawn_x, spawn_y)
+	
+	add_child(obstacle)
 
 func _on_fish_death() -> void:
-	pass # Replace with function body.
+	print("game over!")
+	$ObstacleSpawnTimer.stop()
--- obstacle.gd
+++ obstacle.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-const SPEED = 1
+const SPEED = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta: float) -> void:
-	#position.x -= SPEED
+func _process(_delta: float) -> void:
+	position.x -= SPEED
 
 func _on_body_entered(body: Node2D) -> void:
 	body.on_collision()
