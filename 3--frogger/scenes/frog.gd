add41b6f216a88387fc472fc9f7d548c04c5165a
Fred Chien <fred.chien3@gmail.com>
2025-08-15 20:55:27 -0400

    Implement world boundary

--- 3--frogger/scenes/frog.gd
+++ 3--frogger/scenes/frog.gd
@@ -5,11 +5,26 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _process(delta: float) -> void:
+	var new_pos = position
+	
 	if Input.is_action_just_pressed("move_left"):
-		position.x -= 64
+		new_pos.x -= 64
 	elif Input.is_action_just_pressed("move_right"):
-		position.x += 64
+		new_pos.x += 64
 	elif Input.is_action_just_pressed("move_up"):
-		position.y -= 64
+		new_pos.y -= 64
 	elif Input.is_action_just_pressed("move_down"):
-		position.y += 64
+		new_pos.y += 64
+		
+	if in_bounds(new_pos):
+		position = new_pos
+
+func in_bounds(new_pos) -> bool:
+	if new_pos.x < 0 or new_pos.y < 0:
+		return false
+		
+	var viewport_size = get_viewport().size
+	if new_pos.x >= viewport_size[0] or new_pos.y >= viewport_size[1]:
+		return false
+		
+	return true
