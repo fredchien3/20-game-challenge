9ca303290748b2851ccfb5ba0079919d233448de
Fred Chien <fred.chien3@gmail.com>
2025-08-15 22:43:57 -0400

    Enlarge map, add bounded camera

---
+++ 3--frogger/scenes/camera.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	limit_top = 0
+	limit_left = 0
+	var background = get_tree().get_first_node_in_group("background")
+	var background_size = background.get_rect().size
+	limit_right = background_size.x
+	limit_bottom = background_size.y
--- 3--frogger/scenes/frog.gd
+++ 3--frogger/scenes/frog.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 func _process(delta: float) -> void:
 	var new_pos = position
-	
 	if Input.is_action_just_pressed("move_left"):
 		new_pos.x -= 64
 	elif Input.is_action_just_pressed("move_right"):
@@ -23,8 +21,8 @@ func in_bounds(new_pos) -> bool:
 	if new_pos.x < 0 or new_pos.y < 0:
 		return false
 		
-	var viewport_size = get_viewport().size
-	if new_pos.x >= viewport_size[0] or new_pos.y >= viewport_size[1]:
+	var map_size = get_parent().size
+	if new_pos.x >= map_size[0] or new_pos.y >= map_size[1]:
 		return false
 		
 	return true
