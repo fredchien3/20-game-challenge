77aa88ec7326ed4a5c227b00c4cc5b5bb1e4cf64
Fred Chien <fred.chien3@gmail.com>
2025-09-04 10:24:11 -0400

    Add Player 2 keybindings, second car

--- 4--indy-500/scenes/car/car.gd
+++ 4--indy-500/scenes/car/car.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+@export var player_num: int
+
 const ACCELERATION = 800
 const ROLL_FRICTION = 200
 
@@ -9,7 +11,21 @@ const STEER_LIMIT = 2.5
 const SIDEWAYS_FRICTION = 1
 const DRIFTING_FRICTION = 3
 
-func _process(delta: float) -> void:
+var accelerate_action
+var decelerate_action
+var steer_left_action
+var steer_right_action
+var drift_action
+func _ready() -> void:
+	if player_num == 2:
+		$Body.texture = load("res://scenes/car/car_green.png")
+	accelerate_action = "accelerate_forward_" + str(player_num)
+	decelerate_action = "accelerate_backward_" + str(player_num)
+	steer_left_action = "steer_left_" + str(player_num)
+	steer_right_action = "steer_right_" + str(player_num)
+	drift_action = "drift_" + str(player_num)
+
+func _process(_delta: float) -> void:
 	$Wheels/LeftWheel.rotation_degrees = angular_velocity * 12
 	$Wheels/RightWheel.rotation_degrees = angular_velocity * 12
 
@@ -18,10 +34,10 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	var step := state.get_step()
 
 	# Get inputs
-	var accelerating := Input.is_action_pressed("accelerate_forward")
-	var decelerating := Input.is_action_pressed("accelerate_backward")
-	var steer_direction := Input.get_axis("steer_left", "steer_right")
-	var drifting := Input.is_action_pressed("drift")
+	var accelerating := Input.is_action_pressed(accelerate_action)
+	var decelerating := Input.is_action_pressed(decelerate_action)
+	var steer_direction := Input.get_axis(steer_left_action, steer_right_action)
+	var drifting := Input.is_action_pressed(drift_action)
 	
 	# Handle drift
 	var sideways_friction
@@ -34,7 +50,7 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if accelerating or decelerating:
 		if not drifting:
 			velocity += transform.x \
-				* Input.get_axis("accelerate_backward", "accelerate_forward") \
+				* Input.get_axis(decelerate_action, accelerate_action) \
 				* ACCELERATION * step
 		if steer_direction != 0:
 			if angular_velocity > -STEER_LIMIT and angular_velocity < STEER_LIMIT:
