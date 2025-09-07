9aef3522420504b5eb70a4910c0feffa2466db0d
Fred Chien <fred.chien3@gmail.com>
2025-09-07 17:14:59 -0400

    Add lap counter

--- 4--indy-500/scenes/main/main.gd
+++ 4--indy-500/scenes/main/main.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 const TRACK_PATHS = ["res://scenes/tracks/big_o.tscn", "res://scenes/tracks/lazy_eight.tscn"]
 
+var laps_complete = 0
+var laps_needed = 3
+
 func _ready() -> void:
 	$MainMenu/TrackSelector.get_popup().id_pressed.connect(_on_track_selected)
 	
 	# debug
-	#_on_track_selected(0)
+	_on_track_selected(0)
 
 func _process(_delta: float) -> void:
 	pass
@@ -14,17 +17,25 @@ func _process(_delta: float) -> void:
 func _on_track_selected(id):
 	# Load track
 	var track = load(TRACK_PATHS[id]).instantiate()
+	track.finish_line_crossed.connect(_on_finish_line_crossed)
 	add_child(track)
 	
 	# Load car, attach camera
 	var car = load("res://scenes/car/car.tscn").instantiate()
 	car.player_num = 1
-	#$GameCamera.reparent(car)
-	
-	print(track.get_spawn_position)
-	
+	$GameCamera.reparent(car)
+
 	car.position = track.get_spawn_position()
 	car.rotation_degrees = -90
 	add_child(car)
 
 	$MainMenu.visible = false
+	
+	$HUD/LapCounter.text = str(laps_complete) + "/" + str(laps_needed) + " laps"
+	$HUD/LapCounter.visible = true
+
+func _on_finish_line_crossed(_body):
+	laps_complete += 1
+	$HUD/LapCounter.text = str(laps_complete) + "/" + str(laps_needed) + " laps"
+	if laps_complete == 3:
+		print("you win")
--- 4--indy-500/scenes/tracks/big_o.gd
+++ 4--indy-500/scenes/tracks/big_o.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal finish_line_crossed(body: Node2D)
+
+var finish_line_enabled = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,10 +10,25 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func get_spawn_position():
 	$SpawnPath/PathFollow2D.progress_ratio = randf_range(0, 1)
 	return $SpawnPath/PathFollow2D.global_position
+
+
+func _on_finish_line_body_entered(body: Node2D) -> void:
+	if finish_line_enabled:
+		finish_line_enabled = false
+		finish_line_crossed.emit(body)
+		$HalfwayLine/ArrowYellow.visible = true
+		$FinishLine/ArrowYellow.visible = false
+
+
+func _on_halfway_line_body_entered(_body: Node2D) -> void:
+	if !finish_line_enabled:
+		finish_line_enabled = true
+		$HalfwayLine/ArrowYellow.visible = false
+		$FinishLine/ArrowYellow.visible = true
