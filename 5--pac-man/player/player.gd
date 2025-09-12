dce97d11919f96582eccea56bb77d40566128888
Fred Chien <fred.chien3@gmail.com>
2025-09-12 10:04:26 -0400

    Improve player movement

--- 5--pac-man/player/player.gd
+++ 5--pac-man/player/player.gd
@@ -2,23 +2,22 @@ extends Node2D
 
 const SPEED = 60
 
-var current_direction
-var queued_direction
-var feelers
+@onready var current_direction = null
+@onready var queued_direction = null
+@onready var feelers = {
+	"up": $UpArea,
+	"down": $DownArea,
+	"left": $LeftArea,
+	"right": $RightArea,
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	feelers = {
-		"up": $UpArea,
-		"down": $DownArea,
-		"left": $LeftArea,
-		"right": $RightArea,
-	}
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if queued_direction && queued_direction != current_direction:
-		print(feelers[queued_direction])
 		if feelers[queued_direction].get_overlapping_bodies().is_empty():
 			current_direction = queued_direction
 	
@@ -42,20 +41,24 @@ func _input(event):
 	if event.is_action_pressed("move_up"):
 		if $UpArea.get_overlapping_bodies().is_empty():
 			current_direction = "up"
+			queued_direction = null
 		else:
 			queued_direction = "up"
 	elif event.is_action_pressed("move_down"):
 		if $DownArea.get_overlapping_bodies().is_empty():
 			current_direction = "down"
+			queued_direction = null
 		else:
 			queued_direction = "down"
 	elif event.is_action_pressed("move_left"):
 		if $LeftArea.get_overlapping_bodies().is_empty():
 			current_direction = "left"
+			queued_direction = null
 		else:
 			queued_direction = "left"
 	elif event.is_action_pressed("move_right"):
 		if $RightArea.get_overlapping_bodies().is_empty():
 			current_direction = "right"
+			queued_direction = null
 		else:
 			queued_direction = "right"
