89fbefc0e4122f2ae54885a52d8eb96b2913eb84
Fred Chien <fred.chien3@gmail.com>
2025-09-14 22:56:24 -0400

    Fill map with dots

--- 5--pac-man/main.gd
+++ 5--pac-man/main.gd
@@ -6,14 +6,15 @@ var high_score
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	init_variables()
-	
+	call_deferred("connect_dot_signals")
+
+func connect_dot_signals():
 	for dot in get_tree().get_nodes_in_group("dots"):
 		dot.obtained.connect(_on_dot_obtained)
 	
 func init_variables():
 	score = 0
 	high_score = 0
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
