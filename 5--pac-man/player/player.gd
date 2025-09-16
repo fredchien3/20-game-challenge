c09a1e68f914b7aef9aeeafa595dee4e7ffd199b
Fred Chien <fred.chien3@gmail.com>
2025-09-16 09:13:31 -0400

    Refactor player movement code

--- 5--pac-man/player/player.gd
+++ 5--pac-man/player/player.gd
@@ -5,7 +5,8 @@ const TILE_SIZE = 16
 
 @onready var current_direction = null
 @onready var queued_direction = null
-@onready var feelers = {
+
+@onready var feelers: Dictionary[String, Area2D] = {
 	"up": $UpArea,
 	"down": $DownArea,
 	"left": $LeftArea,
@@ -18,68 +19,57 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if queued_direction && queued_direction != current_direction:
-		if feelers[queued_direction].get_overlapping_bodies().is_empty():
+	if (queued_direction and
+		queued_direction != current_direction and
+		can_move_towards(queued_direction)):
+			snap_to_grid()
 			current_direction = queued_direction
+			queued_direction = null
 	
 	match current_direction:
 		"up":
-			if $UpArea.get_overlapping_bodies().is_empty():
+			if can_move_towards("up"):
 				position.y -= SPEED * delta
 			else:
 				snap_to_grid()
+				current_direction = null
 		"down":
-			if $DownArea.get_overlapping_bodies().is_empty():
+			if can_move_towards("down"):
 				position.y += SPEED * delta
 			else:
 				snap_to_grid()
+				current_direction = null
 		"left":
-			if $LeftArea.get_overlapping_bodies().is_empty():
+			if can_move_towards("left"):
 				position.x -= SPEED * delta
 			else:
 				snap_to_grid()
+				current_direction = null
 		"right":
-			if $RightArea.get_overlapping_bodies().is_empty():
+			if can_move_towards("right"):
 				position.x += SPEED * delta
 			else:
 				snap_to_grid()
+				current_direction = null
 		_:
 			pass
-			
-	for body in $BodyArea.get_overlapping_bodies():
-		if body.is_in_group("walls"):
-			print('collision')
-			snap_to_grid()
 
 func _input(event):
-	if event.is_action_pressed("move_up"):
-		if $UpArea.get_overlapping_bodies().is_empty():
-			current_direction = "up"
-			queued_direction = null
-		else:
-			queued_direction = "up"
-	elif event.is_action_pressed("move_down"):
-		if $DownArea.get_overlapping_bodies().is_empty():
-			current_direction = "down"
-			queued_direction = null
-		else:
-			queued_direction = "down"
-	elif event.is_action_pressed("move_left"):
-		if $LeftArea.get_overlapping_bodies().is_empty():
-			current_direction = "left"
-			queued_direction = null
-		else:
-			queued_direction = "left"
-	elif event.is_action_pressed("move_right"):
-		if $RightArea.get_overlapping_bodies().is_empty():
-			current_direction = "right"
-			queued_direction = null
-		else:
-			queued_direction = "right"
+	if event.is_action_pressed("move_up") and current_direction != "up":
+		queued_direction = "up"
+	elif event.is_action_pressed("move_down") and current_direction != "down":
+		queued_direction = "down"
+	elif event.is_action_pressed("move_left") and current_direction != "left":
+		queued_direction = "left"
+	elif event.is_action_pressed("move_right") and current_direction != "right":
+		queued_direction = "right"
 
 func snap_to_grid():
 	position.x = snapped(position.x, TILE_SIZE / 2)
 	position.y = snapped(position.y, TILE_SIZE / 2)
+		
+func can_move_towards(direction: String) -> bool:
+	return feelers[direction].get_overlapping_bodies().is_empty()
 
 func _on_body_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("dots"):
