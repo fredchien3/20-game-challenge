8e3a9ade43ebcdc734e6b3db5a9b9ebd746d4af3
Fred Chien <fred.chien3@gmail.com>
2025-10-30 19:59:10 -0400

    Make player sprite interactive
    - Animation, flipping, and inversion

--- 6--vvvvvv/player/player.gd
+++ 6--vvvvvv/player/player.gd
@@ -3,24 +3,37 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
-@onready var gravity_multiplier = 1.0
+@onready var gravity_multiplier = 0.4
 
 func get_modified_gravity():
 	return get_gravity() * gravity_multiplier
+	
+func flip_gravity():
+	gravity_multiplier = -gravity_multiplier
+	up_direction = Vector2.DOWN if up_direction == Vector2.UP else Vector2.UP
+	var current_scale = $AnimationPlayer.scale
+	$AnimationPlayer.scale = Vector2(1, -current_scale.y)
 
 func _physics_process(delta: float) -> void:
 	velocity += get_modified_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept"):
-		gravity_multiplier = -gravity_multiplier
-		print(gravity_multiplier)
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		flip_gravity()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		var current_scale = $AnimationPlayer.scale
+		$AnimationPlayer.scale = Vector2(direction * 1, current_scale.y)
+		
+		if is_on_floor():
+			$AnimationPlayer.animation = "running"
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$AnimationPlayer.animation = "idle"
+	
+	if not is_on_floor():
+		$AnimationPlayer.animation = "falling"
 
 	move_and_slide()
