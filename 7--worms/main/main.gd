9c26a3a0a2612f735b276056900d8447b98a4c2c
Fred Chien <fred.chien3@gmail.com>
2026-01-26 21:48:51 -0500

    Init Worm
    - Remove test clipper

--- 7--worms/main/main.gd
+++ 7--worms/main/main.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@export var worm: CharacterBody2D
+
+#func _input(event):
+	#if event.is_action_pressed("shoot"):
+		#worm
+		#
--- 7--worms/main/terrain.gd
+++ 7--worms/main/terrain.gd
@@ -1,29 +1,27 @@
 extends Node
 
-@export var test_clipper: Polygon2D
 @onready var collision := $CollisionPolygon2D
 @onready var terrain := $Polygon2D
 
 func _ready() -> void:
-	test_clipper.connect("clip", _on_poly_clip)
 	collision.polygon = terrain.polygon
 	collision.global_transform = terrain.global_transform
 
-func _on_poly_clip():
-	# Local polygons need to be converted to global space to interact accurately
-	var globalized_terrain_polygon = \
-		terrain.global_transform * terrain.polygon
-
-	var globalized_clipper_polygon = \
-		test_clipper.global_transform * test_clipper.polygon
-	
-	var new_polygons := Geometry2D.clip_polygons(
-		globalized_terrain_polygon,
-		globalized_clipper_polygon
-	)
-
-	# Global space polygons need to be converted back to local space
-	# For now, we just take the leftmost polygon
-	# Most likely later we will write code to take the largest polygon
-	terrain.polygon = terrain.global_transform.affine_inverse() * new_polygons[0]
-	collision.polygon = terrain.polygon
+#func _on_poly_clip():
+	## Local polygons need to be converted to global space to interact accurately
+	#var globalized_terrain_polygon = \
+		#terrain.global_transform * terrain.polygon
+#
+	#var globalized_clipper_polygon = \
+		#test_clipper.global_transform * test_clipper.polygon
+	#
+	#var new_polygons := Geometry2D.clip_polygons(
+		#globalized_terrain_polygon,
+		#globalized_clipper_polygon
+	#)
+#
+	## Global space polygons need to be converted back to local space
+	## For now, we just take the leftmost polygon
+	## Most likely later we will write code to take the largest polygon
+	#terrain.polygon = terrain.global_transform.affine_inverse() * new_polygons[0]
+	#collision.polygon = terrain.polygon
