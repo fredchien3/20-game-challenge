1266235e1eaf7a6751b44380858d27b969b67956
Fred Chien <fred.chien3@gmail.com>
2026-02-12 10:27:06 -0500

    Implement variable power grenade throw

--- 7--worms/worm/worm.gd
+++ 7--worms/worm/worm.gd
@@ -2,13 +2,40 @@ extends CharacterBody2D
 
 signal grenade_thrown(grenade)
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+# Movement
+const SPEED := 150.0
+const JUMP_VELOCITY := -250.0
 
+# Aiming/charging
+@export var power_rate: float
+@export var max_power: float
+# Weapon scenes
 @export var GrenadeScene: PackedScene
+
+@onready var power := 0.0
 
 
 func _physics_process(delta: float) -> void:
+	handle_movement(delta)
+	handle_input(delta)
+
+	# debug
+	if power > 0:
+		$DebugLabel.text = str(int(power))
+
+
+# Event-based input handling
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and not event.is_pressed():
+		# To find a vector pointing from A to B, use B - A.
+		var a = position
+		var b = event.position
+		var vector = (b - a).normalized() * power
+		throw_grenade(vector)
+		power = 0
+
+
+func handle_movement(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -26,15 +53,16 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-# To find a vector pointing from A to B, use B - A.
-func _input(event):
-	if event.is_action_pressed("shoot"):
-		var b = event.position
-		var a = position
-		var vector = b - a
-		throw_grenade(vector)
+# Polling-based input handling
+func handle_input(delta: float) -> void:
+	# Charge up weapon power
+	if Input.is_action_pressed("shoot"):
+		if power < max_power:
+			power += power_rate * delta
 
 
+## Instantiates a grenade with the correct position and velocity, emits it
+## for parent to handle
 func throw_grenade(vector: Vector2):
 	var grenade = GrenadeScene.instantiate()
 	grenade.position = position
