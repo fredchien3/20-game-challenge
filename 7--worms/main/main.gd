45440441459022dfc456e814e8660f352d3de5d1
Fred Chien <fred.chien3@gmail.com>
2026-02-13 13:34:18 -0500

    Add ability to cycle through multiple worms

--- 7--worms/main/main.gd
+++ 7--worms/main/main.gd
@@ -1,3 +1,39 @@
 extends Node2D
 
 @export var terrain: StaticBody2D
+@export var camera: Camera2D
+
+var active_worm: CharacterBody2D
+var active_worm_index := 0
+var worms: Array[Node]
+
+
+func _ready() -> void:
+	worms = get_tree().get_nodes_in_group("worms")
+	worms.sort_custom(left_to_right)
+
+	active_worm = worms[active_worm_index]
+	active_worm.active = true
+	camera.global_position = active_worm.global_position
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_p"):
+		cycle_active_worm()
+
+
+func cycle_active_worm():
+	active_worm.active = false
+	active_worm_index += 1
+
+	if active_worm_index >= len(worms):
+		active_worm_index = 0
+
+	active_worm = worms[active_worm_index]
+	active_worm.active = true
+	camera.global_position = active_worm.global_position
+
+
+func left_to_right(a, b):
+	if a.global_position < b.global_position:
+		return true
--- 7--worms/worm/worm.gd
+++ 7--worms/worm/worm.gd
@@ -14,20 +14,29 @@ const JUMP_VELOCITY := -300.0
 @export var GrenadeScene: PackedScene
 @export var BazookaScene: PackedScene
 
+## Whether it is this worm's turn to move and shoot
+var active = false
+
 @onready var power := 0.0
 
 
 func _physics_process(delta: float) -> void:
-	handle_movement(delta)
-	handle_input(delta)
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-	# debug
-	if power > 0:
-		$DebugLabel.text = str(int(power))
+	if active:
+		handle_movement_input()
+		handle_weapon_input(delta)
+
+	move_and_slide()
+	handle_debug()
 
 
 # Event-based input handling
 func _input(event: InputEvent) -> void:
+	if not active:
+		return
+
 	if event is InputEventMouseButton and not event.is_pressed():
 		# To find a vector pointing from A to B, use B - A.
 		var a = position
@@ -38,10 +47,18 @@ func _input(event: InputEvent) -> void:
 		power = 0
 
 
-func handle_movement(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func handle_debug():
+	# debug
+	if power > 0:
+		$DebugPower.text = str(int(power))
 
+	if active:
+		$DebugActive.visible = true
+	else:
+		$DebugActive.visible = false
+
+
+func handle_movement_input() -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -53,11 +70,8 @@ func handle_movement(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
 
-
-# Polling-based input handling
-func handle_input(delta: float) -> void:
+func handle_weapon_input(delta: float) -> void:
 	# Charge up weapon power
 	if Input.is_action_pressed("shoot"):
 		if power < max_power:
