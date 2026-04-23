e264bf2c83b68b4fca16c761e60e42661d666f68
Fred Chien <fred.chien3@gmail.com>
2026-04-23 10:27:55 -0400

    Add Enemy which dies on projectile hit

--- 8--star-fox/projectile/projectile.gd
+++ 8--star-fox/projectile/projectile.gd
@@ -4,3 +4,12 @@ extends RigidBody3D
 
 func _ready() -> void:
 	linear_velocity = speed
+	await get_tree().create_timer(10).timeout
+	queue_free()
+
+
+func _on_hitbox_area_body_entered(body: Node3D) -> void:
+	if body.has_method("hit_by_projectile"):
+		body.hit_by_projectile()
+
+	queue_free()
