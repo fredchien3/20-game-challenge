fba198f9a8edbd067dd53321fa48a65160cffcc6
Fred Chien <fred.chien3@gmail.com>
2026-04-26 22:52:28 -0400

    Make aimer collide with walls, make projectiles not
    - Adjust aimer and body speeds

--- 8--star-fox/ship/aimer.gd
+++ 8--star-fox/ship/aimer.gd
@@ -1,16 +1,15 @@
-extends MeshInstance3D
+extends RigidBody3D
 
 @export var speed: float
 
 @onready var vertical_speed := speed * 0.85
 
 func _physics_process(_delta):
-	print(vertical_speed)
 	if Input.is_action_pressed("move_right"):
-		global_position.x += speed
+		linear_velocity.x += speed
 	if Input.is_action_pressed("move_left"):
-		global_position.x -= speed
+		linear_velocity.x -= speed
 	if Input.is_action_pressed("move_up"):
-		global_position.y += vertical_speed
+		linear_velocity.y += vertical_speed
 	if Input.is_action_pressed("move_down"):
-		global_position.y -= vertical_speed
+		linear_velocity.y -= vertical_speed
--- 8--star-fox/ship/ship.gd
+++ 8--star-fox/ship/ship.gd
@@ -8,6 +8,6 @@ signal projectile_shot(projectile)
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		var projectile: RigidBody3D = ProjectileScene.instantiate()
-		projectile.global_position = body.global_position
+		projectile.position = body.global_position 
 		projectile.basis = body.basis
 		projectile_shot.emit(projectile)
