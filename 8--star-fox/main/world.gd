70de2197515cacbef7750a24e9ce754bdf667e4b
Fred Chien <fred.chien3@gmail.com>
2026-04-30 10:44:07 -0400

    Tweak rail

--- 8--star-fox/main/world.gd
+++ 8--star-fox/main/world.gd
@@ -1,18 +1,16 @@
 extends Node3D
 
+@export var rail_follow: PathFollow3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var ship = get_tree().get_nodes_in_group("ship")[0]
-	ship.projectile_shot.connect(_on_ship_projectile_shot)
-
+	pass
 
 func _physics_process(delta: float) -> void:
-	var ratio_delta = 0.0005
+	var ratio_per_second = 0.01 * delta
+	rail_follow.progress_ratio += ratio_per_second
+	
 	if Input.is_action_pressed("ui_right"):
-		$Rail/RailFollow.progress_ratio += ratio_delta
+		$Rail/RailFollow.progress_ratio += ratio_per_second * 2
 	if Input.is_action_pressed("ui_left"):
-		$Rail/RailFollow.progress_ratio -= ratio_delta
-
-func _on_ship_projectile_shot(projectile: RigidBody3D):
-	add_child(projectile)
+		$Rail/RailFollow.progress_ratio -= ratio_per_second * 2
