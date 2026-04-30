52a54d4cf9b09946a122146b1cf8c9ab84ed8836
Fred Chien <fred.chien3@gmail.com>
2026-04-30 10:42:16 -0400

    Fix projectiles stuck moving globally forward (instead of their own facing forward)

--- 8--star-fox/projectile/projectile.gd
+++ 8--star-fox/projectile/projectile.gd
@@ -1,12 +1,13 @@
 extends RigidBody3D
 
-@export var speed: float
+@export var speed: float = 20.0
 
 func _ready() -> void:
-	linear_velocity = -global_transform.basis.z * speed
 	await get_tree().create_timer(10).timeout
 	queue_free()
 
+func _physics_process(delta: float) -> void:
+	linear_velocity = -basis.z * speed
 
 func _on_hitbox_area_body_entered(body: Node3D) -> void:
 	if body.has_method("hit_by_projectile"):
--- 8--star-fox/ship/ship.gd
+++ 8--star-fox/ship/ship.gd
@@ -1,13 +1,17 @@
 extends Node3D
 
-signal projectile_shot(projectile)
-
 @export var ProjectileScene: PackedScene
+@export var aimer: RigidBody3D
 @export var body: CharacterBody3D
-
+ 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
+		# The projectile only needs to know two things:
+		# Where its origin is, and what direction to face (normalized).
+		var shoot_dir: Vector3 = (aimer.global_position - body.global_position).normalized()
 		var projectile: RigidBody3D = ProjectileScene.instantiate()
-		projectile.position = body.global_position 
-		projectile.basis = body.basis
-		projectile_shot.emit(projectile)
+		get_parent().add_child(projectile)
+		# We can only set globalized properties after it's been added
+		# to the tree.
+		projectile.global_position = body.global_position
+		projectile.look_at(body.global_position + shoot_dir)
